7d5c1feb804f9e7d153a741986f2e2c8f4bdf996
loius@github.knirsch.de <loius@github.knirsch.de>
2026-09-13 21:06:21 +0200

    Add support for dev profile

--- run-filescanner.sh
+++ run-filescanner.sh
@@ -7,6 +7,9 @@ if [[ -z "${MARIADB_PASSWORD}" ]]; then echo "Please provide .env file with MARI
 if [ ! -f ./docker-compose-filescanner.yml ]; then
     wget https://raw.githubusercontent.com/LoiusCypher/photoview-tools/refs/heads/main/docker-compose-filescanner.yml
 fi
+export HOSTNAME && sudo docker compose -f docker-compose-filescanner.yml down --remove-orphans
 sudo docker pull ghcr.io/loiuscypher/photoview-tools:main
+#export HOSTNAME && sudo docker compose -f docker-compose-filescanner.yml --profile dev up --remove-orphans filescanner_dev
 export HOSTNAME && sudo docker compose -f docker-compose-filescanner.yml up --remove-orphans filescanner
+
 
